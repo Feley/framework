7c3dfcfea2536972f5aa46f04908e5cfb1d6ecfe
Emmanuel [netesy] Olisah <netesy1@gmail.com>
2020-03-27 18:40:07 +0100

    Edited the Demo Page

--- core/resources/view/site/layouts/index.blade.php
+++ core/resources/view/site/layouts/index.blade.php
@@ -27,14 +27,8 @@
                 </div>
                  @yield('content')
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://netesy.github.io/docs">Docs</a>
+                    <a href="https://github.com/netesy/voom">GitHub</a>
                 </div>
             </div>
         </div>
